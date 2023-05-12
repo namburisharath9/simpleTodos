2fe183800bdc9cd12b444d2f36754fe7338cc1ea
NAMBURI SAI SHARATH TRIVEDI <namburisharath9@gmail.com>
2023-05-12 12:49:58 +0000

    simpleTodos

---
+++ src/components/SimpleTodos/index.js
@@ -0,0 +1,73 @@
+import {Component} from 'react'
+import TodoItem from './components/TodoItem'
+
+import './index.css'
+
+const initialTodosList = [
+  {
+    id: 1,
+    title: 'Book the ticket for today evening',
+  },
+  {
+    id: 2,
+    title: 'Rent the movie for tomorrow movie night',
+  },
+  {
+    id: 3,
+    title: 'Confirm the slot for the yoga session tomorrow morning',
+  },
+  {
+    id: 4,
+    title: 'Drop the parcel at Bloomingdale',
+  },
+  {
+    id: 5,
+    title: 'Order fruits on Big Basket',
+  },
+  {
+    id: 6,
+    title: 'Fix the production issue',
+  },
+  {
+    id: 7,
+    title: 'Confirm my slot for Saturday Night',
+  },
+  {
+    id: 8,
+    title: 'Get essentials for Sunday car wash',
+  },
+]
+
+// Write your code here
+
+class SimpleTodos extends Component {
+  state = {todosList: initialTodosList}
+
+  deleteUser = id => {
+    const filteredTodoList = todosList.filter(
+      eachTodoItem => eachTodoItem.id !== id,
+    )
+    this.setState({todosList: filteredTodoList})
+  }
+
+  render() {
+    return (
+      <div className="bg-container">
+        <div className="simple-todos">
+          <h1 className="heading">Simple Todos</h1>
+          <ul className="list-container">
+            {todosList.map(todoDetails => (
+              <TodoItem
+                todoDetails={todoDetails}
+                key={todoDetails.id}
+                deleteUser={this.deleteUser}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SimpleTodos
---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,21 @@
+// Write your code here
+
+const TodoItem = props => {
+  const {tododetails, deleteUser} = props
+  const {id, title} = tododetails
+
+  onDelete = () => {
+    deleteUser(id)
+  }
+
+  return (
+    <li className="list-item">
+      <p className="description">{title}</p>
+      <button className="button" type="button" onClick={onDelete}>
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default TodoItem
